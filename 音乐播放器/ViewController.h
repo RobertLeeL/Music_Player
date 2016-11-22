e6edd3d04ba8301e2b273799fb707a2d01582314
RobertLeeL <RobertLee@robertleedemacbook-pro.local>
2016-11-22 10:38:29 +0800

    Initial Commit

---
+++ 音乐播放器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  音乐播放器
+//
+//  Created by 李龙跃 on 16/11/22.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 音乐播放器/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  音乐播放器
+//
+//  Created by 李龙跃 on 16/11/22.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
